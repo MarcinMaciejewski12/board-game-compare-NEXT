b80d3fd5edc281f802a6ace09e25db50483b26f7
Marcin <maciejewskim118@gmail.com>
2024-07-09 06:36:09 +0200

    feat: score sheet is generated dynamically

--- next-compare/src/app/(pages)/dashboard/scoreboard/[id]/page.tsx
+++ next-compare/src/app/(pages)/dashboard/scoreboard/[id]/page.tsx
@@ -12,47 +12,95 @@ const MOCKED_TABLE = {
     { fieldName: "Wonders", fieldColor: "white" },
     { fieldName: "", fieldColor: "yellow" },
     { fieldName: "", fieldColor: "purple" },
-    { fieldName: "", fieldColor: "gold" },
     { fieldName: "", fieldColor: "green" },
     { fieldName: "", fieldColor: "blue" },
   ],
 };
+
 export default function Scoreboard() {
-  const [score, setScore] = useState();
-  const [playerName, setPlayerName] = useState("");
-  const [playerCount, setPlayerCount] = useState<JSX.Element[]>([]);
-  const playerHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setPlayerName(e.target.value);
+  const [playerCount, setPlayerCount] = useState<number>(0);
+  const [playerInputs, setPlayerInputs] = useState<
+    Array<{ [key: string]: string }>
+  >([]);
+
+  const addPlayer = () => {
+    setPlayerCount(playerCount + 1);
+    setPlayerInputs([...playerInputs, {}]);
   };
-  const playersInputHandler = () => {
-    const inputValue = (
-      <input
-        className="border border-black h-10"
-        value={playerName}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => playerHandler(e)}
-        placeholder="player name"
-      />
-    );
-    setPlayerCount((prevState) => [...prevState, inputValue]);
+
+  const handleInputChange = (
+    playerIndex: number,
+    fieldName: string,
+    value: string,
+  ) => {
+    const newPlayerInputs = [...playerInputs];
+    newPlayerInputs[playerIndex] = {
+      ...newPlayerInputs[playerIndex],
+      [fieldName]: value,
+    };
+    setPlayerInputs(newPlayerInputs);
   };
 
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full overflow-auto">
       <header className="h-48 flex items-center justify-center">
         <h1 className="text-[100px] text-default font-extrabold">
           {MOCKED_TABLE.gameName}
         </h1>
       </header>
       <main className="flex justify-center items-center">
-        <div className="w-[90vw] max-h-full bg-red-500" id="players input">
+        <div
+          className="w-[90vw] max-h-full bg-red-500 overflow-x-auto"
+          id="players input"
+        >
           <div className="flex items-center">
-            <div className="w-28 border border-black bg-white h-10 flex items-center justify-center">
-              <span>Players name:</span>
+            <div className="w-40 border border-black bg-white h-16 flex items-center justify-center p-4">
+              <span>
+                Players name / <br />
+                Game fields
+              </span>
             </div>
-            <div>{playerCount}</div>
-            <button onClick={() => playersInputHandler()}>
+            <div>
+              {Array.from({ length: playerCount }).map((_, playerIndex) => (
+                <input
+                  key={playerIndex}
+                  className="border border-black h-16"
+                  placeholder={`Player ${playerIndex + 1} name`}
+                  onChange={(e) =>
+                    handleInputChange(playerIndex, "name", e.target.value)
+                  }
+                />
+              ))}
+            </div>
+            <button onClick={addPlayer}>
               <Plus className="cursor-pointer" />
             </button>
+          </div>
+          <div>
+            {MOCKED_TABLE.gameScoreBoard.map((item, fieldIndex) => (
+              <div key={fieldIndex} className="flex">
+                <div
+                  className={`w-40 border border-black h-16 flex items-center justify-center`}
+                  style={{ backgroundColor: item.fieldColor }}
+                >
+                  <span>{item.fieldName}</span>
+                </div>
+                {Array.from({ length: playerCount }).map((_, playerIndex) => (
+                  <input
+                    key={playerIndex}
+                    className="border border-black h-16"
+                    placeholder={`Player ${playerIndex + 1} ${item.fieldName}`}
+                    onChange={(e) =>
+                      handleInputChange(
+                        playerIndex,
+                        item.fieldName,
+                        e.target.value,
+                      )
+                    }
+                  />
+                ))}
+              </div>
+            ))}
           </div>
         </div>
       </main>
