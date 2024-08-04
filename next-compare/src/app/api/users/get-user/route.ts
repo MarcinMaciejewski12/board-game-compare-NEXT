9d1e12a85841bb1825d0336f47bbaab802719ad0
Marcin <maciejewskim118@gmail.com>
2024-08-04 20:37:16 +0200

    fix: build

--- next-compare/src/app/api/users/get-user/route.ts
+++ next-compare/src/app/api/users/get-user/route.ts
@@ -3,7 +3,7 @@ import { db } from "@/db";
 import { users } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
-
+const dynamic = 'force-dynamic'
 export async function GET(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
