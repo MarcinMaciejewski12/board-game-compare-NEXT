9a5447c053c5887893f44ed1cb3e4aefc33e92cf
Marcin <maciejewskim118@gmail.com>
2024-10-11 17:07:56 +0200

    refactor: login for mobilephone

--- next-compare/src/app/(directory)/games-list/layout.tsx
+++ next-compare/src/app/(directory)/games-list/layout.tsx
@@ -1,16 +1,7 @@
-import MobileBottomNavigation from "@/components/mobile-bottom-navigation";
-
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <div className="sm:px-5">
-      {children}
-      <div className="w-full fixed bottom-0 z-20 bg-primary h-defaultHeaderHeight sm:hidden">
-        <MobileBottomNavigation />
-      </div>
-    </div>
-  );
+  return <div className="sm:px-5">{children}</div>;
 }
