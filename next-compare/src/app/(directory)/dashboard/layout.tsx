77abd53ec1dfa20399cd663c1a9328b2320ba189
Marcin <maciejewskim118@gmail.com>
2024-12-16 23:33:14 +0100

    fix: height

--- next-compare/src/app/(directory)/dashboard/layout.tsx
+++ next-compare/src/app/(directory)/dashboard/layout.tsx
@@ -1,12 +1,10 @@
-import SvgWave from "@/components/landing-page/svg-wave";
-
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="max-h-[100%] h-full relative">
+    <div className="h-full relative">
       <div>{children}</div>
     </div>
   );
